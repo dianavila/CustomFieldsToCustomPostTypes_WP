aff9b84e20ac90e69d1435c4a46043983357fb74
dianavila <dianavila@gmail.com>
2014-04-02 11:02:28 -0400

    basic documentation

--- create_childrenProfiles.php
+++ create_childrenProfiles.php
@@ -4,26 +4,26 @@ Template Name: create Children porifles
 */
 
 	$args = array( 
-			// 'post_parent' => 357,
-			'p' => 893,
+			
+			'p' => 893, /*post id*/
 			'post_type' => 'page',
-			'posts_per_page' => -1			
+			'posts_per_page' => -1	/*brings all post*/		
 		);
 	$loop = new WP_Query( $args );
 ?>
 <?php while ($loop->have_posts() ) : $loop->the_post(); ?>
 				
-				<h3 style="margin-top:15px;" class="orange-text"><a href="<?php the_permalink();?>"><?php the_title();?></a></h3>
+				<h3 ><a href="<?php the_permalink();?>"><?php the_title();?></a></h3><!--just for confirmation-->
 
 				<?php
-					if(get_field('child_profile'))
+					if(get_field('child_profile'))/* repeater field*/
 	                {
 	                    while(has_sub_field('child_profile')){
 	                        $picture = get_sub_field('picture');
 	                        $name = get_sub_field('name');
 	                        $content = get_sub_field('content');
 
-	                        // insert post
+	                        // preparign parameters to create post in database
 	                        $my_post = array(
 							  'post_title'    => $name,
 							  'post_type'	  => 'children_profile',
@@ -31,15 +31,13 @@ Template Name: create Children porifles
 							  'post_status'   => 'publish',
 							  'post_author'   => 1
 
-							  //'post_category' => array(41)
-							  //'tax_input'      => array('country_profile' => array(41))  // For custom taxonomies. Default empty. 
 							);
 
 							// Insert the post into the database
 							$child_id = wp_insert_post( $my_post );
 							set_post_thumbnail( $child_id, $picture );
-							update_post_meta($child_id, 'country', 'Palestinian Authority');
-							update_post_meta($child_id, 'category', 'Children We Help');
+							update_post_meta($child_id, 'country', '');/*Custom field assigned to this custum post type*/
+							update_post_meta($child_id, 'category', '');/*Custom field assigned to this custum post type*/
 
 						}
 					}
